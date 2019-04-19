46004812692f21dae94b553e5e66e50720ff396c
jpguitron <jpgl1997@gmail.com>
2019-04-19 13:23:46 -0500

    operations + - * /

--- cgen.py
+++ cgen.py
@@ -1,9 +1,15 @@
+#estos dos van en findOperators
+#falta caso a = func()
+#a = b[1]
+
 compare_types = ["+","-","*","/","<","<=",">",">=","==","!=","="]
 class Code_generator:
 
     #Get the name for the file
     def __init__(self, file):
         self.file_name = file
+        self.offset = 0
+        self.variables_directory = {}
 
     #to open the file
     def openFile(self):
@@ -18,33 +24,34 @@ class Code_generator:
         self.file.write(line)
         self.file.write("\n")
 
-variables_directory = {}
-offset = 0
 
 def generateCode(tree,generator):
-    global offset, variables_directory
 
+    #if a int is founded
     if tree.root == "int":
         generator.write("li $a0 0")
         generator.write("sw $a0 0($sp)")
         generator.write("addiu $sp $sp -4")
-        variables_directory[tree.children[0].root] = offset
-        offset += 4
+        generator.variables_directory[tree.children[0].root] = generator.offset
+        generator.offset += 4
 
+    #if output is founded
     elif tree.root == "output":
         output_code(tree.children[0].children[0].children[0].root,generator)
 
+    #if a ["+","-","*","/","<","<=",">",">=","==","!=","="] was founded
     elif findOperators(tree,generator):
         pass
 
+    #if other character is found children of the actual node are called
     elif tree.root not in compare_types:
         for node in tree.children:
             generateCode(node,generator)
 
 #check operators
 def findOperators(tree,generator):
-    global offset, variables_directory
 
+    #if the actual character is in the character list
     if tree.root in compare_types:
 
         leftChild = tree.children[0]
@@ -54,80 +61,110 @@ def findOperators(tree,generator):
         if tree.root == "=":
             #When the rightChild is an integer
             if represents_int(rightChild.root):
-                offset_local = str(offset-variables_directory[leftChild.root])
+                offset_local = str(generator.offset-generator.variables_directory[leftChild.root])
                 generator.write("li $a0 "+str(rightChild.root))
                 generator.write("sw $a0 "+str(offset_local)+"($sp)")
-                
                 
             #when the rightChild is a character
             elif rightChild.root in compare_types:
                 findOperators(rightChild,generator)
                 generator.write("addiu $sp $sp 4")
-                offset -= 4
-                offset_local = str(offset-variables_directory[leftChild.root])
+                generator.offset -= 4
+                offset_local = str(generator.offset-generator.variables_directory[leftChild.root])
                 generator.write("sw $a0 "+str(offset_local)+"($sp)")
-                
 
             #when is equal to another variable
             else: 
                 last_child = tree.children[len(tree.children)-1]
+
+                #if the last child is a int
                 if represents_int(last_child.root):
                     generator.write("li $a0 "+str(last_child.root))
 
+                #if the last child is an arithmetic operator
                 elif last_child.root in compare_types:
                     findOperators(last_child,generator)
                     generator.write("addiu $sp $sp 4")
-                    offset -= 4
-
+                    generator.offset -= 4
+                #if it is a variable
                 else:
-                    offset_local = str(offset-variables_directory[leftChild.root])
+                    offset_local = str(generator.offset-generator.variables_directory[rightChild.root])
                     generator.write("lw $a0 "+str(offset_local)+"($sp)")
-
+                
+                #copy the child to all previus variables
                 for i in range(len(tree.children)-1):
-                    offset_local = str(offset-variables_directory[tree.children[i].root])
+                    offset_local = str(generator.offset-generator.variables_directory[tree.children[i].root])
                     generator.write("sw $a0 "+str(offset_local)+"($sp)")
 
-        #when + operation
-        if tree.root == "+":
-
-            if(represents_int(leftChild.root)):
-                generator.write("li $a0 "+str(leftChild.root))
-                generator.write("sw $a0, 0($sp)")
-                generator.write("addiu $sp $sp -4")
-                offset += 4
-
-            else:
-                findOperators(leftChild,generator)
-
-            if(represents_int(rightChild.root)):
-                generator.write("li $a1 "+str(rightChild.root))
-                generator.write("sw $a1, 0($sp)")
-                generator.write("addiu $sp $sp -4")
-                offset += 4
-            else:
-                findOperators(rightChild,generator)
+        #for arithmetic operators
+        if tree.root == "+" or tree.root == "-" or tree.root == "*" or tree.root == "/":
             
-            offset -= 4
-            generator.write("lw $a0, 4($sp)")
+            #to process the children
+            process_arithmetic_operator(leftChild,generator)
+            process_arithmetic_operator(rightChild,generator)
             
-            offset -= 4
-            generator.write("lw $a1, 8($sp)")
+            #load the last two variables
+            generator.write("lw $a1, 4($sp)")
+            generator.write("lw $a0, 8($sp)")
 
-            generator.write("add $a0 $a0 $a1")
+            #make the operation
+            choose_operation(tree.root,generator)
+
+            #free the last 8 spaces of the stack
             generator.write("addiu $sp $sp 8")
+            generator.offset -= 8
 
+            #save the result in the stack
             generator.write("sw $a0, 0($sp)")
             generator.write("addiu $sp $sp -4")
-            offset += 4
-        pass
+            generator.offset += 4
+        return True
+    return False
 
-    #elif represents_int(tree.root):
+#process child for arithmetic operators
+def process_arithmetic_operator(child,generator):
+
+    #if the child is a int
+    if(represents_int(child.root)):
+        generator.write("li $a0 "+str(child.root))
+        generator.write("sw $a0, 0($sp)")
+        generator.write("addiu $sp $sp -4")
+        generator.offset += 4
+
+    #if the child is an operator
+    elif child.root in compare_types:
+        findOperators(child,generator)
+
+    #if the child is a variable
+    else:
+        offset_local = str(generator.offset-generator.variables_directory[child.root])
+        generator.write("lw $a0 "+str(offset_local)+"($sp)")
+        generator.write("sw $a0, 0($sp)")
+        generator.write("addiu $sp $sp -4")
+        generator.offset += 4
+
+def choose_operation(operation,generator):
+    if operation == "+":
+        generator.write("add $a0 $a0 $a1")
+
+    elif operation == "-":
+        generator.write("sub $a0 $a0 $a1")
+        
+    elif operation == "*":
+        generator.write("mul $a0 $a0 $a1")
+
+    elif operation == "/":
+        generator.write("div $a0 $a1")  
+        generator.write("mflo $a0")  
 
 #To print te value of a variable
 def output_code(var,generator):
-    offset_local = str(offset-variables_directory[var])
+    offset_local = str(generator.offset-generator.variables_directory[var])
     generator.write("li $v0 1")
     generator.write("lw $a0 "+str(offset_local)+"($sp)")
+    generator.write("syscall")
+    generator.write("li $v0 4")
+    generator.write("la $a0 newline")
     generator.write("syscall")
 
 
@@ -147,6 +184,8 @@ def codeGen(tree,file):
         generator.openFile()
 
         #to write the headers in the file
+        generator.write(".data")
+        generator.write('newline: .asciiz "\n"')
         generator.write(".text")
         generator.write(".globl main")
         generator.write("main:")
--- file.s
+++ file.s
@@ -1,3 +1,6 @@
+.data
+newline: .asciiz "
+"
 .text
 .globl main
 main:
@@ -13,52 +16,78 @@ addiu $sp $sp -4
 li $a0 1
 sw $a0, 0($sp)
 addiu $sp $sp -4
-li $a1 2
-sw $a1, 0($sp)
+li $a0 2
+sw $a0, 0($sp)
 addiu $sp $sp -4
-lw $a0, 4($sp)
-lw $a1, 8($sp)
+lw $a1, 4($sp)
+lw $a0, 8($sp)
 add $a0 $a0 $a1
 addiu $sp $sp 8
 sw $a0, 0($sp)
 addiu $sp $sp -4
 addiu $sp $sp 4
 sw $a0 12($sp)
-li $a0 5
+li $a0 9
 sw $a0, 0($sp)
 addiu $sp $sp -4
-li $a1 5
-sw $a1, 0($sp)
+li $a0 3
+sw $a0, 0($sp)
 addiu $sp $sp -4
-lw $a0, 4($sp)
-lw $a1, 8($sp)
-add $a0 $a0 $a1
+lw $a1, 4($sp)
+lw $a0, 8($sp)
+div $a0 $a1
+mflo $a0
+addiu $sp $sp 8
+sw $a0, 0($sp)
+addiu $sp $sp -4
+li $a0 4
+sw $a0, 0($sp)
+addiu $sp $sp -4
+li $a0 1
+sw $a0, 0($sp)
+addiu $sp $sp -4
+lw $a1, 4($sp)
+lw $a0, 8($sp)
+sub $a0 $a0 $a1
+addiu $sp $sp 8
+sw $a0, 0($sp)
+addiu $sp $sp -4
+li $a0 4
+sw $a0, 0($sp)
+addiu $sp $sp -4
+lw $a1, 4($sp)
+lw $a0, 8($sp)
+sub $a0 $a0 $a1
+addiu $sp $sp 8
+sw $a0, 0($sp)
+addiu $sp $sp -4
+lw $a1, 4($sp)
+lw $a0, 8($sp)
+mul $a0 $a0 $a1
 addiu $sp $sp 8
 sw $a0, 0($sp)
 addiu $sp $sp -4
 addiu $sp $sp 4
-sw $a0 4($sp)
 sw $a0 8($sp)
-li $a0 5
-sw $a0, 0($sp)
-addiu $sp $sp -4
-li $a1 5
-sw $a1, 0($sp)
-addiu $sp $sp -4
-lw $a0, 4($sp)
-lw $a1, 8($sp)
-add $a0 $a0 $a1
-addiu $sp $sp 8
-sw $a0, 0($sp)
-addiu $sp $sp -4
-li $v0 1
-lw $a0 16($sp)
-syscall
+lw $a0 8($sp)
+sw $a0 4($sp)
 li $v0 1
 lw $a0 12($sp)
 syscall
+li $v0 4
+la $a0 newline
+syscall
 li $v0 1
 lw $a0 8($sp)
+syscall
+li $v0 4
+la $a0 newline
+syscall
+li $v0 1
+lw $a0 4($sp)
+syscall
+li $v0 4
+la $a0 newline
 syscall
 li $v0, 10
 syscall
